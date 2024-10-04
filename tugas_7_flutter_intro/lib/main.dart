c2b11aaa2f8919f545ee8c6c6c2172e442d334f6
Apih <umbarawatirapih@gmail.com>
2024-10-04 17:04:14 +0700

    tugas-9-slacing-detail-news

--- tugas_7_flutter_intro/lib/main.dart
+++ tugas_7_flutter_intro/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tugas_7_flutter_intro/materi/materialAppScaffold.dart';
 import 'package:tugas_7_flutter_intro/tugas-7-flutter-info/counter_app.dart';
 import 'package:tugas_7_flutter_intro/tugas-8/news_page.dart';
+import 'package:tugas_7_flutter_intro/tugas-9/detail_news_app.dart';
 import 'package:tugas_7_flutter_intro/tugas-9/news_app.dart';
 
 void main() {
@@ -21,12 +22,13 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const MyHomePage(title: 'Tugas - 7 Counter Apps'),
-      initialRoute: "/tugas9",
+      initialRoute: "/tugas9Detail",
       routes: {
         "/home": (context) => HomePageNew(),
         "/tugas7": (context) => const MyHomePage(title: 'Tugas - 7 Counter Apps'),
         "/tugas8": (context) => const NewsPage(),
         "/tugas9": (context) => const NewsApp(),
+        "/tugas9Detail": (context) => const DetailNewsApp(),
       }
     );
   }
---
+++ tugas_7_flutter_intro/lib/tugas-9/detail_news_app.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DetailNewsApp extends StatelessWidget {
+  const DetailNewsApp({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: SafeArea(
+        child: Expanded(
+          child: ListView(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 375,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/news6.png'), // Lokasi gambar
+                    fit: BoxFit.cover,
+                    opacity: 63 // Menyesuaikan ukuran gambar dengan container
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.zero,
+                    topRight: Radius.zero,
+                    bottomLeft:Radius.circular(32), 
+                    bottomRight: Radius.circular(32)
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(20),
+                      // width: 40,
+                      // height: 40,
+                      child:  SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: (){},
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(0),
+                            backgroundColor: Colors.black,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                              side: BorderSide(
+                                color: Colors.black,
+                                width: 1
+                              )
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.arrow_back, 
+                            color: Colors.white,
+                            // size: 14,
+                          )
+                        ),
+                      )
+                    ),
+                  ],
+                ), 
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                      Row(
+                        children: [
+                          Container(
+                            width: 40,
+                            height: 40,
+                            decoration: BoxDecoration(
+                              image: const DecorationImage(
+                                image: AssetImage('assets/images/avatar.png'), // Lokasi gambar
+                                fit: BoxFit.contain,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          const SizedBox(width: 15, height: 15),
+                          const Text("Samuel Newton"),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        "TECHNOLOGY",
+                        style: GoogleFonts.poppins(
+                          fontSize: 12, 
+                          fontWeight: FontWeight.w900,
+                          color: const Color.fromARGB(255, 96, 96, 96)
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "To build responsibly, tech needs to do more than just hire chief ethics officers",
+                        style: GoogleFonts.poppins(fontSize: 22, fontWeight: FontWeight.w900),
+                        softWrap: true,
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "17 June, 2023 — 4:49 PM",
+                        style: GoogleFonts.poppins(fontSize: 12, color: const Color.fromARGB(255, 96, 96, 96)),
+                      ),
+                      const SizedBox(height: 10),
+                      const Divider(
+                        color: Color(0x141E2814),
+                        height: 2
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "In the last couple of years, we’ve seen new teams in tech companies emerge that focus on responsible innovation, digital well-being, AI ethics or humane use. Whatever their titles, these individuals are given the task of “leading” ethics at their companies.",
+                        style: GoogleFonts.poppins(fontSize: 16),
+                        softWrap: true,
+                        // overflow: TextOverflow.visible,
+                      ),
+                    ],
+                ),
+              ),
+            ],
+          )
+        )
+      )
+    );
+  }
+}
