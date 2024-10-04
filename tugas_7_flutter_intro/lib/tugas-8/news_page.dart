25490e6fcc234bf78b7b4077297b1c40d942a9fa
Apih <umbarawatirapih@gmail.com>
2024-10-04 17:37:07 +0700

    add padding di tugas-8

--- tugas_7_flutter_intro/lib/tugas-8/news_page.dart
+++ tugas_7_flutter_intro/lib/tugas-8/news_page.dart
@@ -9,43 +9,46 @@ class NewsPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 86, left: 30, right: 30),
-              width: double.infinity,
-              height: 330,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/news_page.png")
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 86, left: 30, right: 30),
+                width: double.infinity,
+                height: 200,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/news_page.png")
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 37),
-            Text(
-              'Yuk, Membaca Bersama\nSanber News',
-              style: GoogleFonts.arimo(fontWeight: FontWeight.bold, fontSize: 21),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 21),
-            Text(
-              'Berita Terpercaya, Di Ujung Jari Anda',
-              style: TextStyle(fontSize: 15),
-            ),
-            Spacer(),
-            CustomButton(
-              btnColor: Colors.blue,
-              textColor: Colors.white,
-              btnText: "Masuk"
-            ),
-            SizedBox(height: 21),
-            CustomButton(
-              btnColor: Colors.white,
-              textColor: Colors.blue,
-              btnText: "Mendaftar"
-            ),
-          ],
-        )
+              const SizedBox(height: 37),
+              Text(
+                'Yuk, Membaca Bersama\nSanber News',
+                style: GoogleFonts.arimo(fontWeight: FontWeight.bold, fontSize: 21),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 21),
+              const Text(
+                'Berita Terpercaya, Di Ujung Jari Anda',
+                style: TextStyle(fontSize: 15),
+              ),
+              const Spacer(),
+              const CustomButton(
+                btnColor: Colors.blue,
+                textColor: Colors.white,
+                btnText: "Masuk"
+              ),
+              const SizedBox(height: 21),
+              const CustomButton(
+                btnColor: Colors.white,
+                textColor: Colors.blue,
+                btnText: "Mendaftar"
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
