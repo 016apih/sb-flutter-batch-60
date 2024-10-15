4d700b17fb2c8986e139de026fb8f26515a0e88f
Apih <umbarawatirapih@gmail.com>
2024-10-15 22:14:52 +0700

    create getX final-project

---
+++ final_project/android/app/src/main/kotlin/com/sbflutterfinalproject/final_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sbflutterfinalproject.final_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ final_project/lib/app/routes/app_pages.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+
+import '../modules/auth/bindings/auth_binding.dart';
+import '../modules/auth/views/auth_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/user/bindings/user_binding.dart';
+import '../modules/user/views/user_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => const AuthView(),
+      binding: AuthBinding(),
+    ),
+    GetPage(
+      name: _Paths.USER,
+      page: () => const UserView(),
+      binding: UserBinding(),
+    ),
+  ];
+}
